71305be4e72950132fdb733d9b70d1ec0add004e
Nicholas Parkes <nickparkes7@gmail.com>
2019-11-14 12:54:31 -0500

    Glove Controller done for demo

--- GloveController.ino
+++ GloveController.ino
@@ -1,8 +1,18 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
+#include <Servo.h>
 
-// Set up WiFi UDP connection
+// Servo and pump pin setup
+Servo servo1;
+Servo servo2; 
+int servo1Pin = 5;
+int servo2Pin = 6;
+// int servo3Pin = 9;
+// int servo4Pin = 10;
+// int pumpPin = 13;
+
+// WiFi UDP connection setup
 const char* ssid = "iPhone";
 const char* password = "mechatronics";
 
@@ -11,26 +21,24 @@ unsigned int receivePort = 2808;                 // Arbitrary port for receiving
 const unsigned int UDP_PACKET_SIZE = 4;          // Size of packets
 byte receiveBuffer[UDP_PACKET_SIZE + 1];         // Packet of joystick data 
 
-// Set IP address
 IPAddress myIP(192, 168, 1, 156);                // glove IP
-
-// Constants for ledc analog writing
-int freq = 150;
-int pump1Pin = 0;
-int pump2Pin = 1;    
-int pump3Pin = 2;
-int pump4Pin = 3;
-int resolution_bits = 8;
-int resolution = ((1<<resolution_bits)-1);
 
 
 void setup() {
+  // Set Pin Mode
+  pinMode(servo1Pin, OUTPUT);    // PWM Signal servo 1
+  pinMode(servo2Pin, OUTPUT);    // PWM Signal servo 2
+  // pinMode(servo3Pin, OUTPUT);    // PWM Signal serv0 3
+  // pinMode(servo4Pin, OUTPUT);    // PWM Signal servo 4
+  // pinMode(pumpPin, OUTPUT);      // Digital out pump
 
-  // Set Pin Modes
-  pinMode(32, OUTPUT);   // PWM Signal pump 1
-  pinMode(22, OUTPUT);   // PWM Signal pump 2
+  // digitalWrite(pumpPin, HIGH);
+  servo1.attach(servo1Pin);
+  servo1.write(0);
+  servo2.attach(servo2Pin);
+  servo2.write(0);
 
-  // Set up serial connection
+  //Set up serial connection
   Serial.begin(115200);
   Serial.print("Connecting to "); Serial.println(ssid);
 
@@ -51,22 +59,27 @@ void setup() {
   receiveBuffer[UDP_PACKET_SIZE] = 0;
 }
 
-
 void loop() {
 
-  // Receive data continuously from VR Interface -- take in as 4 byte string
+  // Receive data continuously from VR Interface -- take in as 1 byte string
   int indicator = 1;
   while (indicator != 0) {
     indicator = receive_byte();
   }
 
   // Remove 10 added before sending
-  byte x_val = receiveBuffer[0] - 10;
-  byte y_val = receiveBuffer[1] - 10;
+  byte received = receiveBuffer[0] - 10
+
+  byte out1 = received && 0b00000001;
+  byte out2 = received && 0b00000010;
+  byte out3 = received && 0b00000100;
+  byte out4 = received && 0b00001000;
 
   // Output values to motor
-  analogWrite(pump1Pin, x_val);
-  analogWrite(pump2Pin, y_val);
+  servo1.write(out1);
+  servo2.write(out2);
+  servo3.write(out3);
+  servo4.write(out4);
 }
 
 
